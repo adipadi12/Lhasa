d51e42c21b401759c6253ec76a2f622b87654941
Aditya Jha <ajha4436@gmail.com>
2026-08-03 00:38:22 +0530

    initial commit

---
+++ ArchitectureDecisions.md
@@ -0,0 +1,3 @@
+data/ & types/ separated instead of constant.ts
+because types describe what exists
+data describes actual content
---
+++ lhasa-lounge/src/types/menu.ts
@@ -0,0 +1,16 @@
+export type DishCategory = // think enum in C#
+  | "Momos"
+  | "Soup"
+  | "Rice"
+  | "Noodles"
+  | "Dessert"
+  | "Drink";
+
+export interface Dish {
+  id: string;
+  name: string;
+  description: string;
+  category: DishCategory;
+  image: string;
+  featured: boolean;
+}
